897ec577380178dbe2cf1bb8313424f8b29c8fc4
namsh1125 <namsh1125@naver.com>
2025-01-07 01:59:26 +0900

    Init: 프로젝트 초기 설정

---
+++ src/main/java/com/woorifisa/reservation/StudyroomReservationApplication.java
@@ -0,0 +1,13 @@
+package com.woorifisa.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyroomReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyroomReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woorifisa/reservation/StudyroomReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woorifisa.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyroomReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
