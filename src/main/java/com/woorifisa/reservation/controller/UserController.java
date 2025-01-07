f7a8b947b0c148aae8f41940fc7285e55264d18d
namsh1125 <namsh1125@naver.com>
2025-01-08 00:46:55 +0900

    Update: 응답 결과 변경 (HTTP 상태 코드 포함 등) 및 코드 리팩토링

--- src/main/java/com/woorifisa/reservation/controller/UserController.java
+++ src/main/java/com/woorifisa/reservation/controller/UserController.java
@@ -1,32 +1,35 @@
 package com.woorifisa.reservation.controller;
 
 import com.woorifisa.reservation.dto.*;
-import com.woorifisa.reservation.entity.User;
-import com.woorifisa.reservation.repository.UserRepository;
 import com.woorifisa.reservation.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/signup")
-    public BaseResponse<SignUpResponseDTO> signup(@RequestBody SignUpRequestDTO request) {
-        SignUpResponseDTO data = userService.signup(request);
-        return new BaseResponse<>(201, "User created successfully", data);
+    public ResponseEntity<BaseResponse<SignUpResponseDTO>> signup(@RequestBody SignUpRequestDTO body) {
+        SignUpResponseDTO data = userService.signup(body);
+        BaseResponse<SignUpResponseDTO> response = new BaseResponse<>(HttpStatus.CREATED.value(), "회원가입에 성공했습니다.", data);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public BaseResponse<LoginResponseDTO> login(@RequestBody LoginRequestDTO request) {
-        LoginResponseDTO data = userService.login(request);
-        return new BaseResponse<>(200, "Login successful", data);
+    public ResponseEntity<BaseResponse<LoginResponseDTO>> login(@RequestBody LoginRequestDTO body) {
+        LoginResponseDTO data = userService.login(body);
+        BaseResponse<LoginResponseDTO> response = new BaseResponse<>(HttpStatus.OK.value(), "로그인에 성공했습니다.", data);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
