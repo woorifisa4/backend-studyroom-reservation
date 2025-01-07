85f9795f952ad544d6129edcf2d1aaebeeaef456
namsh1125 <namsh1125@naver.com>
2025-01-07 23:18:44 +0900

    Hotfix: CORS 설정 수정

--- src/main/java/com/woorifisa/reservation/config/SecurityConfig.java
+++ src/main/java/com/woorifisa/reservation/config/SecurityConfig.java
@@ -8,6 +8,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +24,18 @@ public class SecurityConfig {
         return http
                 .httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic 인증 비활성화.
                 .csrf(AbstractHttpConfigurer::disable) // CSRF 설정 비활성화.
-                .cors(AbstractHttpConfigurer::disable) // CORS 설정 비활성화.
+                .cors(cors -> {
+                    CorsConfigurationSource source = request -> {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(List.of("*")); // 모든 도메인 허용.
+                        config.setAllowedMethods(List.of("*")); // 모든 HTTP 메서드 허용.
+                        config.setAllowedHeaders(List.of("*")); // 모든 헤더 허용.
+                        return config;
+                    };
+
+                    // CORS 설정 적용.
+                    cors.configurationSource(source);
+                })
                 .headers(headers ->
                         headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable) // X-Frame-Options 설정 비활성화.
                 )
@@ -29,4 +45,5 @@ public class SecurityConfig {
                 .build();
 
     }
+
 }
