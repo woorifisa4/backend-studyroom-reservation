bbc28739ccaa81ea122dac041a9eadf66b328e98
namsh1125 <namsh1125@naver.com>
2025-01-12 22:14:04 +0900

    Hotfix: 프로토타입 개발을 위한 CSRF 설정 비활성화

--- src/main/java/com/woorifisa/reservation/config/SecurityConfig.java
+++ src/main/java/com/woorifisa/reservation/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -18,7 +19,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // TODO: 프로토타입 개발을 위해 임시로 CSRF 설정 비활성화 및 CORS 설정을 모두 허용하는 설정을 적용.
+        //  추후 Session 방식과 JWT 방식 중 선택하여 적용할 경우, 해당 방식에 맞게 설정을 변경할 필요가 있음.
+
         return http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 설정 비활성화.
                 .cors(cors -> {
                     CorsConfigurationSource source = request -> {
                         CorsConfiguration config = new CorsConfiguration();
