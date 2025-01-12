b39b39aa0b8a860cb3de4933083c0fcccec7c59f
namsh1125 <namsh1125@naver.com>
2025-01-12 21:43:16 +0900

    Update: Null값인 경우 data 필드를 전송하지 않도록 수정

--- src/main/java/com/woorifisa/reservation/dto/BaseResponse.java
+++ src/main/java/com/woorifisa/reservation/dto/BaseResponse.java
@@ -1,11 +1,14 @@
 package com.woorifisa.reservation.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
 @Getter
 public class BaseResponse<T> {
     private int statusCode;
     private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private T data;
 
     public BaseResponse(int statusCode, String message, T data) {
