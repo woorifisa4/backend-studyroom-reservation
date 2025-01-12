ba15e7a71569fe7f112f6a8cb80eb65238946836
namsh1125 <namsh1125@naver.com>
2025-01-12 21:36:37 +0900

    Update: 불필요한 핸들러 제거 및 MethodArgumentNotValidException 처리 핸들러에 로그 추가

--- src/main/java/com/woorifisa/reservation/exception/GlobalExceptionHandler.java
+++ src/main/java/com/woorifisa/reservation/exception/GlobalExceptionHandler.java
@@ -1,17 +1,22 @@
 package com.woorifisa.reservation.exception;
 
 import com.woorifisa.reservation.dto.BaseResponse;
-import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -39,26 +44,46 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class) // 데이터 타입이 맞지 않을 때 발생하는 예외 처리 용도 (날짜 형식이 맞지 않는 경우를 처리하기 위한 목적으로 사용)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)// 데이터 타입이 맞지 않을 때 발생하는 예외 처리 용도 (날짜 형식이 맞지 않는 경우를 처리하기 위한 목적으로 사용)
     public ResponseEntity<BaseResponse<Void>> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
         BaseResponse<Void> response = new BaseResponse<>(HttpStatus.BAD_REQUEST.value(), "올바르지 않은 요청입니다.", null);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<BaseResponse<Map<String, String>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    @ExceptionHandler(MethodArgumentNotValidException.class) // @Valid로 @RequestBody에 매핑되는 DTO 클래스를 검증할 때 던져지는 예외 처리 용도
+    public ResponseEntity<BaseResponse<Map<String, String>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
         Map<String, String> errors = new HashMap<>();
-        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
-            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        StringBuilder logMessage = new StringBuilder();
+
+        // FieldError를 통해 각 필드의 에러 메시지 및 값 처리
+        for (int i = 0; i < ex.getBindingResult().getFieldErrors().size(); i++) {
+            FieldError fieldError = ex.getBindingResult().getFieldErrors().get(i);
+            String fieldName = fieldError.getField(); // 필드명
+            String errorMessage = fieldError.getDefaultMessage(); // 에러 메시지
+            String fieldValue = (String) fieldError.getRejectedValue(); // 필드의 실제 값 가져오기
+
+            errors.put(fieldName, errorMessage);
+
+            // 마지막 항목이 아니면 콤마 추가
+            logMessage.append(String.format("%s: \"%s\"", fieldName, fieldValue));
+            if (i < ex.getBindingResult().getFieldErrors().size() - 1) {
+                logMessage.append(", ");
+            }
         }
 
+        // 요청 정보 로깅
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        String requestUri = attributes.getRequest().getRequestURI(); // 요청 URL
+        String httpMethod = attributes.getRequest().getMethod(); // HTTP 메소드
+
+        String logMsg = String.format("API에 잘못된 요청이 들어왔습니다. / 오류 메시지: %s / 요청 정보: %s %s {%s}",
+                String.join(" & ", errors.values()), httpMethod, requestUri, logMessage.toString());
+
+        log.warn(logMsg);
+
+        // 응답
         BaseResponse<Map<String, String>> response = new BaseResponse<>(HttpStatus.BAD_REQUEST.value(), "올바르지 않은 요청입니다.", errors);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(ConstraintViolationException.class) //
-    public ResponseEntity<BaseResponse<String>> handleConstraintViolationException(ConstraintViolationException ex) {
-        BaseResponse<String> response = new BaseResponse<>(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), null);
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
 }
