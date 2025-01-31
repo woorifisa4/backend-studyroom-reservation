f7df1093200ede9e543eead4385ea008eefd1196
namsh1125 <namsh1125@naver.com>
2025-01-31 23:50:49 +0900

    Update: JWT 사용으로 세션 생성 정책 수정

--- src/main/java/com/woorifisa/reservation/config/SecurityConfig.java
+++ src/main/java/com/woorifisa/reservation/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -20,7 +21,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // TODO: 프로토타입 개발을 위해 임시로 CSRF 설정 비활성화 및 CORS 설정을 모두 허용하는 설정을 적용.
-        //  추후 Session 방식과 JWT 방식 중 선택하여 적용할 경우, 해당 방식에 맞게 설정을 변경할 필요가 있음.
+        //  CORS origin 수정 및 특정 URL에 대한 접근 제한 등의 설정 필요.
 
         return http
                 .csrf(AbstractHttpConfigurer::disable) // CSRF 설정 비활성화.
@@ -38,6 +39,9 @@ public class SecurityConfig {
                 })
                 .authorizeHttpRequests(request ->
                         request.anyRequest().permitAll() // 임시) 모든 요청에 대해 인증 불필요.
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT 사용으로 세션 비활성화.
                 )
                 .build();
 
