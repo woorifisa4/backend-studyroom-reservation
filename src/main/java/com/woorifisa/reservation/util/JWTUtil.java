2f62c1309b02a9058c874cf5b6eebbfcfbe63356
namsh1125 <namsh1125@naver.com>
2025-02-01 03:40:49 +0900

    Update: JWT에 들어가는 값 수정

--- src/main/java/com/woorifisa/reservation/util/JWTUtil.java
+++ src/main/java/com/woorifisa/reservation/util/JWTUtil.java
@@ -3,6 +3,7 @@ package com.woorifisa.reservation.util;
 import com.woorifisa.reservation.dto.TokenDTO;
 import com.woorifisa.reservation.entity.Role;
 import com.woorifisa.reservation.entity.User;
+import com.woorifisa.reservation.repository.UserRepository;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
@@ -19,19 +20,33 @@ public class JWTUtil {
 
     private final Key secretKey;
 
+    private final String issuer;
+
+    private final String audience;
+
     private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 3; // 3시간
 
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 24 * 7; // 7일
 
-    public JWTUtil(@Value("${jwt.secret}") String secret) {
+    private final UserRepository userRepository;
+
+    public JWTUtil(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.issuer}") String issuer,
+            @Value("${jwt.audience}") String audience,
+            UserRepository userRepository
+    ) {
         this.secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+        this.issuer = issuer;
+        this.audience = audience;
+        this.userRepository = userRepository;
     }
 
     public TokenDTO generateTokens(User user) {
-        return generateTokens(user.getId(), user.getName(), user.getRole());
+        return generateTokens(user.getId(), user.getRole());
     }
 
-    public TokenDTO generateTokens(Long userId, String userName, Role userRole) {
+    public TokenDTO generateTokens(Long userId, Role userRole) {
         Date now = new Date();
         Date accessTokenExpiry = new Date(now.getTime() + ACCESS_TOKEN_EXPIRE_TIME);
         Date refreshTokenExpiry = new Date(now.getTime() + REFRESH_TOKEN_EXPIRE_TIME);
@@ -39,31 +54,31 @@ public class JWTUtil {
         return TokenDTO.builder()
                 .tokenType("Bearer")
                 .role(userRole)
-                .accessToken(createAccessToken(userId, userName, userRole, accessTokenExpiry))
+                .accessToken(createAccessToken(userId, userRole, accessTokenExpiry))
                 .accessTokenExpiresAt(accessTokenExpiry)
-                .refreshToken(createRefreshToken(userId, userName, refreshTokenExpiry))
+                .refreshToken(createRefreshToken(userId, refreshTokenExpiry))
                 .refreshTokenExpiresAt(refreshTokenExpiry)
                 .build();
     }
 
-    public String createAccessToken(Long userId, String userName, Role userRole, Date accessTokenExpiry) {
+    public String createAccessToken(Long userId, Role userRole, Date accessTokenExpiry) {
         return Jwts.builder()
-                // Header: 토큰의 타입(JWT)과 서명에 사용된 알고리즘(HS512) 정보를 담는다.
+                // Header: 토큰의 타입(JWT)를 담는다. 서명에 사용된 알고리즘의 경우 signWith 메소드에서 자동으로 설정된다.
                 .header()
                 .add("typ", "JWT") // 토큰의 타입 지정. 여기서는 JWT를 사용.
-                .add("alg", "HS512") // 서명 알고리즘 지정. 여기서는 HMAC SHA-512를 사용.
                 .and()
 
-                // Payload: 토큰에 담을 클레임(데이터)을 지정. 클레임에는 사용자의 이름, 역할, ID 등이 포함될 수 있다.
-                .issuer("WOORIFISA-STUDY-ROOM-RESERVATION-SERVER") // iss 클레임: 토큰 발급자를 지정
-                .subject(userName) // sub 클레임: 토큰 제목을 지정
-//                .audience().add("your-audience").and()  // aud 클레임: 토큰 대상자를 지정
+                // Payload: 토큰에 담을 클레임(데이터)을 지정한다. 클레임에는 사용자의 이름, 역할, ID 등이 포함될 수 있다.
+                .issuer(issuer) // iss 클레임: 토큰 발급자를 지정
+                .subject(String.valueOf(userId)) // sub 클레임: 토큰의 주제. 보통 사용자 ID를 지정
+                .audience() // aud 클레임: 토큰 대상자를 지정.
+                .add(audience)
+                .and()
                 .expiration(accessTokenExpiry) // exp 클레임: 토큰 만료 시간을 지정
 //                .notBefore(Date(System.currentTimeMillis())) // nbf 클레임: 토큰 활성 날짜를 지정. 헤딩 시간 이전에는 토큰이 활성화되지 않는다.
                 .issuedAt(new Date()) // iat 클레임: 토큰 발급 시간을 지정
 //                .id(UUID.randomUUID().toString()) // jti 클레임: JWT 토큰 식별자를 지정
                 .claim("role", userRole) // 사용자 정의 클레임: 사용자의 역할
-                .claim("client-id", userId) // 사용자 정의 클레임: 사용자의 식별자
 
                 // Signature: header와 payload를 암호화한 결과. 이 부분이 토큰의 무결성을 보장하는 부분
                 .signWith(secretKey) // signWith 메소드를 사용해 서명 알고리즘과 키를 지정
@@ -72,15 +87,27 @@ public class JWTUtil {
                 .compact();
     }
 
-    public String createRefreshToken(Long userId, String userName, Date refreshTokenExpiry) {
+    public String createRefreshToken(Long userId, Date refreshTokenExpiry) {
         return Jwts.builder()
-                .subject(userName) // sub 클레임: 토큰 제목을 지정
-                .issuer("WOORIFISA-STUDY-ROOM-RESERVATION-SERVER") // iss 클레임: 토큰 발급자를 지정
-                .issuedAt(new Date()) // iat 클레임: 토큰 발급 시간을 지정
+                // Header: 토큰의 타입(JWT)를 담는다. 서명에 사용된 알고리즘의 경우 signWith 메소드에서 자동으로 설정된다.
+                .header()
+                .add("typ", "JWT") // 토큰의 타입 지정. 여기서는 JWT를 사용.
+                .and()
+
+                // Payload: 토큰에 담을 클레임(데이터)을 지정한다. Access Token과 달리 Refresh Token에서는 간단한 정보만 포함한다.
+                .issuer(issuer) // iss 클레임: 토큰 발급자를 지정
+                .subject(String.valueOf(userId)) // sub 클레임: 토큰의 주제. 보통 사용자 ID를 지정
+                .audience() // aud 클레임: 토큰 대상자를 지정.
+                .add(audience)
+                .and()
                 .expiration(refreshTokenExpiry) // exp 클레임: 토큰 만료 시간을 지정
-                .claim("client-id", userId) // 사용자 정의 클레임: 사용자의 식별자
+                .issuedAt(new Date()) // iat 클레임: 토큰 발급 시간을 지정
+
+                // Signature: header와 payload를 암호화한 결과. 이 부분이 토큰의 무결성을 보장하는 부분
                 .signWith(secretKey) // signWith 메소드를 사용해 서명 알고리즘과 키를 지정
-                .compact(); // JWT 토큰 문자열을 생성
+
+                // compact 메소드를 호출해 JWT 토큰 문자열을 생성한다.
+                .compact();
     }
 
     public TokenDTO refreshTokens(String refreshToken) {
@@ -98,12 +125,14 @@ public class JWTUtil {
             }
 
             // 클레임에서 사용자 정보 추출
-            String name = claims.getSubject();
-            Long userId = claims.get("client-id", Long.class);
-            Role role = Role.valueOf(claims.get("role", String.class));
+            Long userId = Long.parseLong(claims.getSubject());
+
+            // 사용자 정보 조회
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new JwtException("사용자 정보를 찾을 수 없습니다."));
 
             // 새로운 토큰 발급
-            return generateTokens(userId, name, role);
+            return generateTokens(user);
 
         } catch (ExpiredJwtException e) {
             log.warn("만료된 리프레시 토큰으로 재발급을 시도했습니다.");
